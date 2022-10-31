20c196e7dc81b2a2b7db26a3a713bd33e63e6e13
YannGUIOT <guiot_yann@hotmail.fr>
2022-10-31 16:33:13 +0100

    Second commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/contact', to: 'contact#contact'
+  get '/equipe', to: 'team#team'
+
+  get '/welcome/:id', to: 'welcome#welcome'
+
 end
