1ea94da2c671fb1d69933724804822a56729eb9c
YannGUIOT <guiot_yann@hotmail.fr>
2022-11-01 22:31:09 +0100

    pusssh

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-
-  # Defines the root path route ("/")
-
-
   resources :gossip
-  resources :users
+  resources :user
  
-
   root 'gossip#index'
-  #get '/accueil', to: 'accueil#index'
 
   get '/contact', to: 'contact#contact'
   get '/equipe', to: 'team#team'
 
   get '/welcome/:id', to: 'welcome#welcome'
   get '/welcome', to: 'welcome#welcome'
-
-  # get '/user/:id', to: 'user#user'
-  # get '/gossip/:id', to: 'gossip#gossip'
-
-  # get '/new', to: 'gossip#new'
-  
-
-
 
 end
